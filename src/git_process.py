6ba396a013db75b0f19eb86063f38e0393f0581a
Pavel Shiltsov <pd.shiltsov@gmail.com>
2026-05-15 16:37:08 +0200

    TUI interface improvements

---
+++ src/git_process.py
@@ -0,0 +1,49 @@
+from dataclasses import dataclass
+import subprocess
+
+
+@dataclass
+class Commit:
+    _hash: str
+    _parents: list[str]
+    _author: str
+    _date: str
+    _msg: str
+    
+
+def get_commits():
+    result = subprocess.run(
+        [
+            "git",
+            "log",
+            "--all",
+            "--pretty=format:%H|%P|%an|%ad|%s"
+        ],
+        capture_output=True,
+        text=True
+    )
+
+    commits = []
+
+    for line in result.stdout.splitlines():
+        commit_hash, parents, author, date, message = line.split("|", 4)
+
+        commits.append(Commit(
+            commit_hash,
+            parents.split(),
+            author,
+            date,
+            message,
+    
+        ))
+
+    return commits[::-1]
+
+
+if __name__ == "__main__":
+    commits = get_commits()
+    for c in commits:
+        print("HASH:", c._hash)
+        print("PARENTS:", c._parents)
+        print("MESSAGE:", c._msg)
+        print()
--- src/tui.py
+++ src/tui.py
@@ -1,3 +1,4 @@
+from git_process import Commit, get_commits
 import curses
 
 # Color configuration
@@ -21,6 +22,10 @@ def init_colors() -> None:
     curses.init_pair(TEXT_PAIR, TEXT_FG, TEXT_BG)
     curses.init_pair(STATUS_PAIR, STATUS_FG, STATUS_BG)
 
+
+def commit_render(commit: Commit) -> str:
+    return f"{commit._hash} - {commit._date} - {commit._author}"
+    
 def draw_status_bar(stdscr, text: str) -> None:
     h, w = stdscr.getmaxyx()
     y = h - 2
@@ -30,29 +35,38 @@ def draw_status_bar(stdscr, text: str) -> None:
     stdscr.attroff(curses.color_pair(STATUS_PAIR))
 
 
-def render(stdscr, info: str) -> None:
+def render(stdscr, commits: list[Commit], pos: int) -> None:
     stdscr.erase()
 
     h, w = stdscr.getmaxyx()
 
     stdscr.attron(curses.color_pair(TEXT_PAIR))
-    stdscr.addstr(2, 2, info)
+    # stdscr.addstr(0, 2, commits.__repr__())
+    limit = min(h - 2 - 2, len(commits))
+    
+    for i in range(0, limit):
+        if i == (pos % limit):
+            stdscr.addstr(i + 2, 2, commit_render(commits[i]), curses.A_REVERSE)
+        else:
+            stdscr.addstr(i + 2, 2, commit_render(commits[i]))
+            
     stdscr.attroff(curses.color_pair(TEXT_PAIR))
 
-    draw_status_bar(stdscr, " (q to exit)")
+    # status bar
+    draw_status_bar(stdscr, f" (q to exit) pos: {pos}, h: {h}, w: {w}")
 
     stdscr.refresh()
-
 
 def main(stdscr) -> None:
     curses.curs_set(0)
     stdscr.keypad(True)
 
     init_colors()
+    commits = get_commits()
 
     y = 0
     while True:
-        render(stdscr, f"Hello, world! {y}")
+        render(stdscr, commits, y)
 
         key = stdscr.getch()
 
