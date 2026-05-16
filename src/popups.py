416a86dbe5dadfae129aab5ca2751885d23eae8f
Pavel Shiltsov <pd.shiltsov@gmail.com>
2026-05-16 21:37:53 +0200

    more addons

--- src/popups.py
+++ src/popups.py
@@ -2,11 +2,24 @@ from git_process import Commit
 import curses
 
 
+# TODO: Remove popup windows due to their unstable behaviour
 def draw_commit_info_popup(stdscr, commit: Commit):
     h, w = stdscr.getmaxyx()
 
     height = 10
     width = 40
+
+    max_width = min(60, width - 2)
+    msg = commit._msg
+
+    lines = []
+    for line in msg.splitlines():
+        while len(line) > max_width:
+            lines.append(line[:max_width])
+            line = line[max_width:]
+
+        if line:
+            lines.append(line)
 
     y = (h - height) // 2
     x = (w - width) // 2
@@ -14,7 +27,8 @@ def draw_commit_info_popup(stdscr, commit: Commit):
     win = curses.newwin(height, width, y, x)
     win.box()
 
-    win.addstr(1, 1, commit._msg)
-    # win.addstr(5, 2, "(q to exit)")
+    # TODO: add pager
+    for i in range(0, len(lines)):
+        win.addstr(i + 1, 1, lines[i])
 
     return win
